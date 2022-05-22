43723c2f6b80987f3301ee924b435e15c9e76e3b
Ilia Mogilevsky <ilia.mogilevsky@astutepayroll.com>
2022-05-22 10:12:32 +1000

    Adding save button and pin to localstorage, adding 'raw' date to view render

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Search for Articles</h1>
         <ArticleAutoSuggest />
       </header>
     </div>
