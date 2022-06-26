097a0f2dd06716d0881311c340b95520221579f9
Garry Yeatman <garryyeatman@Garrys-MacBook-Pro-2.local>
2022-06-26 20:56:02 +1000

    Setting up project and adding react

---
+++ news-app/news-search-app/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Test
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
