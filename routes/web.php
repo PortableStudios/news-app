4c2ebd530479921d2b7a5e602c9b9be856fdb4ae
Raja <raja.kcpal@gmail.com>
2023-08-09 05:47:45 +0545

    Default route added to news list.

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,20 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/', [NewsController::class, 'index'])->name('news.index');
 Route::get('/news/search', [NewsController::class, 'search'])->name('news.search');
 Route::post('/news/bookmark', [NewsController::class, 'bookmark'])->name('news.bookmark');
 
